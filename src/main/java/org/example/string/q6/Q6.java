12771a05b7e4ac751bf13022e945182043f67888
dilith <dilith914@gmail.com>
2023-11-04 22:00:29 +0530

    str q6

---
+++ src/main/java/org/example/string/q6/Q6.java
@@ -0,0 +1,15 @@
+package org.example.string.q6;
+
+public class Q6 {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        String str = "     -91283472332-43535 with words";
+
+        int x = solution.myAtoi(str);
+
+        System.out.println(x);
+
+    }
+}
