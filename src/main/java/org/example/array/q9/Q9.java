5b42565de2bed5b5a62b7a2ac2d4d7c01bcfcf1b
dilith <dilith914@gmail.com>
2023-11-04 17:09:03 +0530

    arr q9

---
+++ src/main/java/org/example/array/q9/Q9.java
@@ -0,0 +1,13 @@
+package org.example.array.q9;
+
+public class Q9 {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] nums = new int[]{2,7,11,15};
+        int[] x = solution.twoSum(nums, 9);
+        System.out.println(x);
+
+    }
+}
