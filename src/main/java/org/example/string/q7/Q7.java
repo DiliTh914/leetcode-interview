b8fa4e8944bf6ada09b9b93eeb76565907094f1e
dilith <dilith914@gmail.com>
2023-11-04 22:09:35 +0530

    str q7

---
+++ src/main/java/org/example/string/q7/Q7.java
@@ -0,0 +1,13 @@
+package org.example.string.q7;
+
+public class Q7 {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int x = solution.strStr("sadbutsad","sad");
+
+        System.out.println(x);
+
+    }
+}
