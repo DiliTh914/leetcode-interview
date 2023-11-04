55e86497c5a677942531ffef71aeef5d74da1b2f
dilith <dilith914@gmail.com>
2023-11-04 17:54:58 +0530

    arr q10 2nd solution

--- src/main/java/org/example/array/q10/Solution.java
+++ src/main/java/org/example/array/q10/Solution.java
@@ -95,4 +95,91 @@ class Solution {
 
         return true;
     }
+
+    /**
+     *
+     *  bit more faster and less mem required
+     */
+    public boolean isValidSudoku2(char[][] board) {
+
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> column = new HashSet<>();
+
+        Set<Integer> sq1 = new HashSet<>();
+        Set<Integer> sq2 = new HashSet<>();
+        Set<Integer> sq3 = new HashSet<>();
+
+        Set<Integer> sq4 = new HashSet<>();
+        Set<Integer> sq5 = new HashSet<>();
+        Set<Integer> sq6 = new HashSet<>();
+
+        Set<Integer> sq7 = new HashSet<>();
+        Set<Integer> sq8 = new HashSet<>();
+        Set<Integer> sq9 = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int x = board[i][j];
+                if (x!=46) {
+                    if (!row.add(x-48)) {
+                        return false;
+                    }
+                }
+
+                int y = board[j][i];
+                if (y!=46) {
+                    if (!column.add(y-48)) {
+                        return false;
+                    }
+                }
+                int z = board[i][j];
+                if (z!=46) {
+                    if (i<3 && j<3) {
+                        if (!sq1.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<6 && j<3) {
+                        if (!sq2.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<9 && j<3) {
+                        if (!sq3.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<3 && j<6) {
+                        if (!sq4.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<6 && j<6) {
+                        if (!sq5.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<9 && j<6) {
+                        if (!sq6.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<3 && j<9) {
+                        if (!sq7.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<6 && j<9) {
+                        if (!sq8.add(z-48)) {
+                            return false;
+                        }
+                    } else if (i<9 && j<9) {
+                        if (!sq9.add(z-48)) {
+                            return false;
+                        }
+                    }
+
+                }
+
+            }
+            row.clear();
+            column.clear();
+
+        }
+
+        return true;
+    }
 }
