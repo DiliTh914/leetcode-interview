505b2521b4cb2617d0f4284f0512872ffbdf3b77
dilith <dilith914@gmail.com>
2023-11-06 01:24:58 +0530

    tree q3 init

---
+++ src/main/java/org/example/tree/q3/Q.java
@@ -0,0 +1,14 @@
+package org.example.tree.q3;
+
+public class Q {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        String[] strs = {"ab", "a"};
+        boolean x = solution.isSymmetric(null);
+
+        System.out.println(x);
+
+    }
+}
---
+++ src/main/java/org/example/tree/q3/Solution.java
@@ -0,0 +1,21 @@
+package org.example.tree.q3;
+
+public class Solution {
+
+    /**
+     * Q
+     * <a href="https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/627/">...</a>
+     * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+     *
+     * Input: root = [1,2,2,3,4,4,3]
+     * Output: true
+     *
+     * Input: root = [1,2,2,null,3,null,3]
+     * Output: false
+     */
+    public boolean isSymmetric(TreeNode root) {
+
+    }
+
+
+}
