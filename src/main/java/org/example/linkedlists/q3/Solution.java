9bb3f2b6f33911fa05c45ced49386fd6631baaf8
dilith <dilith914@gmail.com>
2023-11-05 12:17:25 +0530

    ll q4

--- src/main/java/org/example/linkedlists/q3/Solution.java
+++ src/main/java/org/example/linkedlists/q3/Solution.java
@@ -3,7 +3,7 @@ package org.example.linkedlists.q3;
 public class Solution {
 
     /**
-     * Q2:
+     * Q3:
      * Given the head of a singly linked list, reverse the list, and return the reversed list.
      *
      * Input: head = [1,2,3,4,5]
