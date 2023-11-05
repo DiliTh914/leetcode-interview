cf24993126e20eeb6b981624750d5f743fe03ba8
dilith <dilith914@gmail.com>
2023-11-05 10:39:07 +0530

    ll q1

---
+++ src/main/java/org/example/linkedlists/q1/ListNode.java
@@ -0,0 +1,9 @@
+package org.example.linkedlists.q1;
+
+public class ListNode {
+      int val;
+      ListNode next;
+      public ListNode(int x) {
+          this.val = x;
+      }
+ }
---
+++ src/main/java/org/example/linkedlists/q1/Q1.java
@@ -0,0 +1,24 @@
+package org.example.linkedlists.q1;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        ListNode head = new ListNode(2);
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(3);
+
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        solution.deleteNode(head);
+
+
+    }
+}
