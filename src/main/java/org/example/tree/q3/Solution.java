a8941c2dc41e16bf9c9381100f9520430c3f5653
dilith <dilith914@gmail.com>
2023-11-07 21:28:19 +0530

    tree q3

--- src/main/java/org/example/tree/q3/Solution.java
+++ src/main/java/org/example/tree/q3/Solution.java
@@ -1,5 +1,7 @@
 package org.example.tree.q3;
 
+import java.util.List;
+
 public class Solution {
 
     /**
@@ -14,7 +16,24 @@ public class Solution {
      * Output: false
      */
     public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        else return getTree(root.left, root.right);
 
+    }
+
+
+
+    private boolean getTree(TreeNode treeNode1, TreeNode treeNode2) {
+        if (treeNode1 == null && treeNode2==null) {
+            return true;
+        } else if (treeNode1 == null || treeNode2==null) {
+            return false;
+        }
+
+        if (treeNode1.val == treeNode2.val) {
+            if (!getTree(treeNode1.left, treeNode2.right)) return false;
+            return getTree(treeNode1.right, treeNode2.left);
+        } else return false;
     }
 
 
