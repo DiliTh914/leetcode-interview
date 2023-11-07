95f99c471012e988d31bdc7c8cf7b0f236a7212b
dilith <dilith914@gmail.com>
2023-11-07 22:26:58 +0530

    sort q1

---
+++ src/main/java/org/example/sortsearch/q1/Q.java
@@ -0,0 +1,17 @@
+package org.example.sortsearch.q1;
+
+public class Q {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] arr1 = new int[]{};
+        int m =3;
+        int[] arr2 = new int[]{};
+        int n =3;
+        solution.merge(arr1, m, arr2, n);
+
+
+    }
+}
